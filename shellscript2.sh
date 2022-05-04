c8c9a237838de84052ceb83e6818d103711760a5
BecomeDevops <84931173+BecomeDevops@users.noreply.github.com>
2022-05-04 14:12:16 +0530

    Add two Numbers

---
+++ shellscript2.sh
@@ -0,0 +1,12 @@
+ADD TWO NUMBERS
+
+#!/bin/bash
+
+echo -e "enter the name of file :"
+read file_name 
+if [ -e $nagesh ]
+then
+     echo "$file_name found" 
+else 
+     echo "$file_name not found"
+fi
