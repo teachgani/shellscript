c6d364fb27deeabb94ea7002b963a50860c03502
BecomeDevops <84931173+BecomeDevops@users.noreply.github.com>
2022-05-04 14:23:21 +0530

    Candidate details

---
+++ shellscript5.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "Hey what is your Name:"
+read name
+echo "wellcome to mr/mrs $name,would you like to tell us last name"
+read lastname
+echo "thanks mr/mrs $name $lastname for telling us your name"
+echo "what is your current company?"
+read companyname
+echo "what is your current package?"
+read salary
+echo "what is your expectation salary?"
+read expctdsal
+echo "prefered locations?"
+read preflocation
+echo "what is your total experience in IT?"
+read experience
+echo " okey mr/mrs $name your profille get shortlisted i will inform you"
+
